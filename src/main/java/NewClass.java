e0a8eb52f4aa433e42bdc704ed75cc02d0ce7d21
jassu19 <jassu19@Hotmail.com>
2021-04-11 10:58:42 -0400

    first Commit

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -3,6 +3,6 @@ public class NewClass {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-        System.out.println("hello");
+        System.out.println("helo");
     }
 }

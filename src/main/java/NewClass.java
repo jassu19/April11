ab1bfe977476078835191292d68aa399c63758a1
jassu19 <jassu19@Hotmail.com>
2021-04-11 11:58:45 -0400

    next

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,11 +1,9 @@
 public class NewClass {
-
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
         System.out.println("helo");
         System.out.println("helo");
-
-
+        System.out.println("Changed");
     }
 }
